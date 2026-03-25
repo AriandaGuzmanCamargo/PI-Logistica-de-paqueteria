60ea51c751377ab1c79f1cc243dce9cd6dbc2217
Luis-Guadalupe-Barron-Duran <124049120@upq.edu.mx>
2026-03-25 10:54:52 -0600

    Dependencias de Expo para mobile

--- Frontend/piMobile/src/config/api.js
+++ Frontend/piMobile/src/config/api.js
@@ -1,8 +1,28 @@
+import Constants from 'expo-constants';
 import { Platform } from 'react-native';
 
 const fromEnv = process.env.EXPO_PUBLIC_API_URL;
 
+const getHostFromExpo = () => {
+  const hostUri =
+    Constants.expoConfig?.hostUri ||
+    Constants.manifest2?.extra?.expoClient?.hostUri ||
+    null;
+
+  if (!hostUri) {
+    return null;
+  }
+
+  return hostUri.split(':')[0];
+};
+
 const getDefaultApiUrl = () => {
+  const host = getHostFromExpo();
+
+  if (host) {
+    return `http://${host}:3001`;
+  }
+
   if (Platform.OS === 'android') {
     return 'http://10.0.2.2:3001';
   }
